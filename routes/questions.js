531e660691e32ae804abed163a10be538e4ccaec
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-04 03:03:20 -0700

    Update questions.js

--- routes/questions.js
+++ routes/questions.js
@@ -11,21 +11,7 @@ router.post('/', async (req, res) => {
       return res.status(400).send('Bad Request: Missing or invalid "entities"');
     }
 
-    // Ensure the incoming entities have all the required fields
-    const questions = entities.map(entity => new Question({
-      chapter: entity.chapter,
-      questionText: entity.questionText,
-      DifficultyLevel: entity.DifficultyLevel,
-      Subject: entity.Subject,
-      Chaptername: entity.Chaptername,
-      ChapterPagenumber: entity.ChapterPagenumber,
-      ImagePath: entity.ImagePath,
-      TableDataPath: entity.TableDataPath,
-      Topic: entity.Topic,
-      QuestionType: entity.QuestionType,
-      BookTitle: entity.BookTitle,
-      Authors: entity.Authors
-    }));
+    const questions = entities.map(entity => new Question(entity));
     await Question.insertMany(questions);
 
     res.status(201).json({ message: 'Questions saved successfully', questions });
