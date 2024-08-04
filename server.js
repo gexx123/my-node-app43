c14b00ca88938a15ca6c4038a1e0d34413d5b86b
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-04 01:32:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const MONGO_URI = 'mongodb+srv://tunwalhimanshu:kCyfmscb2spY14yG@paperbot.6vhle9
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.log('Error connecting to MongoDB:', err));
 
 app.use(express.json());
 
