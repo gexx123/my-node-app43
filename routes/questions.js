3aef253852e9f262e5e220728caf879b38979899
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-07 12:26:41 -0700

    Update questions.js

--- routes/questions.js
+++ routes/questions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Question = require('../question'); // Adjust the path according to your folder structure
+const Question = require('../models/question'); // Adjust the path according to your folder structure
 
 // Route to handle GET request to /api/questions with query parameters for filtering
 router.get('/questions', async (req, res) => {
@@ -23,17 +23,21 @@ router.get('/questions', async (req, res) => {
   }
 });
 
-// Route to handle POST request to /api/questions/filter with JSON body parameters for filtering
+// Route to handle POST request to /api/questions/filter for filtering questions
 router.post('/questions/filter', async (req, res) => {
   try {
-    const { subject, chapter, difficulty, type, topic } = req.body;
+    const { DifficultyLevel, type, Topic, chapter, Subject, ChapterPagenumber, BookTitle, Authors, Class } = req.body;
 
     const query = {};
-    if (subject) query.Subject = subject;
-    if (chapter) query.Chaptername = chapter;
-    if (difficulty) query.DifficultyLevel = difficulty;
+    if (DifficultyLevel) query.DifficultyLevel = DifficultyLevel;
     if (type) query.QuestionType = type;
-    if (topic) query.Topic = topic;
+    if (Topic) query.Topic = Topic;
+    if (chapter) query.Chaptername = chapter;
+    if (Subject) query.Subject = Subject;
+    if (ChapterPagenumber) query.ChapterPagenumber = ChapterPagenumber;
+    if (BookTitle) query.BookTitle = BookTitle;
+    if (Authors) query.Authors = Authors;
+    if (Class) query.Class = Class;
 
     const questions = await Question.find(query);
 
