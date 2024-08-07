bf0a0e70a1d7a155ddcba6e73189af3905b35277
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-07 12:30:07 -0700

    Update questions.js

--- routes/questions.js
+++ routes/questions.js
@@ -1,8 +1,10 @@
+// routes/question.js
+
 const express = require('express');
 const router = express.Router();
 const Question = require('../models/question'); // Adjust the path according to your folder structure
 
-// Route to handle GET request to /api/questions with query parameters for filtering
+// Get all questions
 router.get('/questions', async (req, res) => {
   try {
     const { subject, chapter, difficulty, type, topic } = req.query;
@@ -23,7 +25,7 @@ router.get('/questions', async (req, res) => {
   }
 });
 
-// Route to handle POST request to /api/questions/filter for filtering questions
+// Filter questions via POST
 router.post('/questions/filter', async (req, res) => {
   try {
     const { DifficultyLevel, type, Topic, chapter, Subject, ChapterPagenumber, BookTitle, Authors, Class } = req.body;
@@ -41,9 +43,9 @@ router.post('/questions/filter', async (req, res) => {
 
     const questions = await Question.find(query);
 
-    res.status(200).json({ message: 'Questions retrieved successfully', questions });
+    res.status(200).json({ message: 'Questions filtered successfully', questions });
   } catch (error) {
-    console.error('Error fetching questions:', error);
+    console.error('Error filtering questions:', error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 });
