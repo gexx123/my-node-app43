414a99a6114dbbc95373edc4009f7d31196bb315
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-15 05:52:49 -0700

    Update RouteQuestion.js

--- routes/RouteQuestion.js
+++ routes/RouteQuestion.js
@@ -5,20 +5,7 @@ const ClassModel = require('../models/ModelQuestion');
 // Route to retrieve documents based on query parameters
 router.get('/questions', async (req, res) => {
   try {
-    const { 
-      className, 
-      subjectName, 
-      chapterName, 
-      questionText,
-      difficultyLevel,
-      topic,
-      questionType,
-      bookTitle,
-      authors,
-      chapterPageNumber,
-      imagePath,
-      tableDataPath 
-    } = req.query;
+    const { className, subjectName, chapterName, questionText } = req.query;
 
     const query = {};
 
@@ -27,47 +14,15 @@ router.get('/questions', async (req, res) => {
     }
 
     if (subjectName) {
-      query['subjects'] = { $elemMatch: { subjectName: subjectName } };
+      query['subjects.subjectName'] = subjectName;
     }
 
     if (chapterName) {
-      query['subjects.chapters'] = { $elemMatch: { chapterName: chapterName } };
+      query['subjects.chapters.chapterName'] = chapterName;
     }
 
     if (questionText) {
-      query['subjects.chapters.questions'] = { $elemMatch: { questionText: questionText } };
-    }
-
-    if (difficultyLevel) {
-      query['subjects.chapters.questions.metaData.difficultyLevel'] = difficultyLevel;
-    }
-
-    if (topic) {
-      query['subjects.chapters.questions.metaData.topic'] = topic;
-    }
-
-    if (questionType) {
-      query['subjects.chapters.questions.metaData.questionType'] = questionType;
-    }
-
-    if (bookTitle) {
-      query['subjects.chapters.questions.metaData.bookTitle'] = bookTitle;
-    }
-
-    if (authors) {
-      query['subjects.chapters.questions.metaData.authors'] = authors;
-    }
-
-    if (chapterPageNumber) {
-      query['subjects.chapters.questions.metaData.chapterPageNumber'] = chapterPageNumber;
-    }
-
-    if (imagePath) {
-      query['subjects.chapters.questions.metaData.resources.imagePath'] = imagePath;
-    }
-
-    if (tableDataPath) {
-      query['subjects.chapters.questions.metaData.resources.tableDataPath'] = tableDataPath;
+      query['subjects.chapters.questions.questionText'] = questionText;
     }
 
     const classes = await ClassModel.find(query);
