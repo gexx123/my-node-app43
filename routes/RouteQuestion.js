062fa1a4216f596e0f062276603f9312094ac827
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-15 05:11:21 -0700

    Update RouteQuestion.js

--- routes/RouteQuestion.js
+++ routes/RouteQuestion.js
@@ -2,16 +2,31 @@ const express = require('express');
 const router = express.Router();
 const ClassModel = require('../models/ModelQuestion');
 
-// Route to retrieve questions based on className
+// Route to retrieve documents based on query parameters
 router.get('/questions', async (req, res) => {
   try {
-    const { className } = req.query;
+    const { className, subjectName, chapterName, questionText } = req.query;
 
     const query = {};
-    if (className) query.className = className;
+
+    if (className) {
+      query.className = className;
+    }
+
+    if (subjectName) {
+      query['subjects.subjectName'] = subjectName;
+    }
+
+    if (chapterName) {
+      query['subjects.chapters.chapterName'] = chapterName;
+    }
+
+    if (questionText) {
+      query['subjects.chapters.questions.questionText'] = questionText;
+    }
 
     const classes = await ClassModel.find(query);
-    
+
     res.status(200).json({ message: 'Questions retrieved successfully', classes });
   } catch (error) {
     console.error('Error fetching questions:', error);
