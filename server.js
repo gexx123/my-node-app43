adffc8e1e6097e7768aa26e2e30794defcddf8e4
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-15 09:26:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // MongoDB connection string
-const MONGO_URI = 'Your_MongoDB_URI_Here'; // Replace with your actual MongoDB URI
+const MONGO_URI = 'mongodb+srv://tunwalhimanshu:kCyfmscb2spY14yG@paperbot.6vhle9d.mongodb.net/schoolData?retryWrites=true&w=majority&appName=paperbot';
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
