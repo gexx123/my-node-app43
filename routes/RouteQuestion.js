9686386c039824fd9a6747f08a34ead3e522bfdd
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-15 04:02:39 -0700

    Update RouteQuestion.js

--- routes/RouteQuestion.js
+++ routes/RouteQuestion.js
@@ -2,25 +2,16 @@ const express = require('express');
 const router = express.Router();
 const ClassModel = require('../models/ModelQuestion');
 
-// Route to retrieve all documents
+// Route to retrieve questions based on className
 router.get('/questions', async (req, res) => {
   try {
-    const classes = await ClassModel.find({});
-    console.log('Retrieved Classes:', JSON.stringify(classes, null, 2));  // Log retrieved classes
-
-    res.status(200).json({ message: 'Questions retrieved successfully', classes });
-  } catch (error) {
-    console.error('Error fetching questions:', error);
-    res.status(500).json({ error: 'Internal Server Error', details: error.message });
-  }
-});
-
-// Route to retrieve questions based on className
-router.get('/questions/filter', async (req, res) => {
-  try {
     const { className } = req.query;
-    const classes = await ClassModel.find({ className });
 
+    const query = {};
+    if (className) query.className = className;
+
+    const classes = await ClassModel.find(query);
+    
     res.status(200).json({ message: 'Questions retrieved successfully', classes });
   } catch (error) {
     console.error('Error fetching questions:', error);
