f2d98e962709177c260440c24558b1b2cdc2dfc6
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-15 21:17:09 +0530

    Update ModelQuestion.js

--- models/ModelQuestion.js
+++ models/ModelQuestion.js
@@ -2,43 +2,43 @@ const mongoose = require('mongoose');
 
 // Define the schema for resources
 const resourcesSchema = new mongoose.Schema({
-  imagePath: String,
-  tableDataPath: String
-}, { _id: false });  // _id: false to prevent creating _id for subdocuments
+  imagePath: { type: String, default: "None" },
+  tableDataPath: { type: String, default: "None" }
+}, { _id: false });
 
 // Define the schema for metadata
 const metaDataSchema = new mongoose.Schema({
-  difficultyLevel: String,
-  topic: String,
-  questionType: String,
-  bookTitle: String,
-  authors: String,
-  chapterPageNumber: String,
-  resources: resourcesSchema
+  difficultyLevel: { type: String, required: true },
+  topic: { type: String, required: true },
+  questionType: { type: String, required: true },
+  bookTitle: { type: String, required: true },
+  authors: { type: String, required: true },
+  chapterPageNumber: { type: String, required: true },
+  resources: { type: resourcesSchema, required: true }
 }, { _id: false });
 
 // Define the schema for questions
 const questionSchema = new mongoose.Schema({
-  questionText: String,
-  metaData: metaDataSchema
+  questionText: { type: String, required: true },
+  metaData: { type: metaDataSchema, required: true }
 }, { _id: false });
 
 // Define the schema for chapters
 const chapterSchema = new mongoose.Schema({
-  chapterName: String,
-  questions: [questionSchema]
+  chapterName: { type: String, required: true },
+  questions: { type: [questionSchema], required: true }
 }, { _id: false });
 
 // Define the schema for subjects
 const subjectSchema = new mongoose.Schema({
-  subjectName: String,
-  chapters: [chapterSchema]
+  subjectName: { type: String, required: true },
+  chapters: { type: [chapterSchema], required: true }
 }, { _id: false });
 
 // Define the schema for class
 const classSchema = new mongoose.Schema({
-  className: String,
-  subjects: [subjectSchema]
+  className: { type: String, required: true },
+  subjects: { type: [subjectSchema], required: true }
 });
 
 const ClassModel = mongoose.model('ClassModel', classSchema);
