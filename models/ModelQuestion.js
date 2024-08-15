3436673be8384739ba726bf934e08a062c350aba
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-15 04:02:18 -0700

    Update ModelQuestion.js

--- models/ModelQuestion.js
+++ models/ModelQuestion.js
@@ -1,37 +1,36 @@
 const mongoose = require('mongoose');
 
-// Define the Question Schema
 const questionSchema = new mongoose.Schema({
-  questionText: String,
-  metaData: {
-    difficultyLevel: String,
-    topic: String,
-    questionType: String,
-    bookTitle: String,
-    authors: String,
-    chapterPageNumber: String,
-    resources: {
-      imagePath: String,
-      tableDataPath: String
-    }
-  }
-});
-
-const chapterSchema = new mongoose.Schema({
-  chapterName: String,
-  questions: [questionSchema]
-});
-
-const subjectSchema = new mongoose.Schema({
-  subjectName: String,
-  chapters: [chapterSchema]
-});
-
-const classSchema = new mongoose.Schema({
   className: String,
-  subjects: [subjectSchema]
+  subjects: [
+    {
+      subjectName: String,
+      chapters: [
+        {
+          chapterName: String,
+          questions: [
+            {
+              questionText: String,
+              metaData: {
+                difficultyLevel: String,
+                topic: String,
+                questionType: String,
+                bookTitle: String,
+                authors: String,
+                chapterPageNumber: String,
+                resources: {
+                  imagePath: String,
+                  tableDataPath: String
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
 });
 
-const ClassModel = mongoose.model('ClassModel', classSchema);
+const ClassModel = mongoose.model('ClassModel', questionSchema);
 
 module.exports = ClassModel;
