f1e9e42646ebdd5e586550bf5803239df742cc30
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-15 05:11:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const routeQuestion = require('./routes/RouteQuestion');
+const QuestionRoutes = require('./routes/RouteQuestion');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const MONGO_URI = 'mongodb+srv://tunwalhimanshu:kCyfmscb2spY14yG@paperbot.6vhle9d.mongodb.net/schoolData?retryWrites=true&w=majority&appName=paperbot';
+// MongoDB connection string
+const MONGO_URI = 'your-mongodb-uri-here';
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.log('MongoDB connection error:', err));
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', routeQuestion);
+app.use('/api', QuestionRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
