da44fbf301c3f1fc5aa143a45b4777e5b0b8e5a7
gexx123 <133238256+gexx123@users.noreply.github.com>
2024-08-15 02:41:43 -0700

    Update question.js

--- models/question.js
+++ models/question.js
@@ -1,37 +1,44 @@
 const mongoose = require('mongoose');
 
-// Define your Mongoose schema and model
-const questionSchema = new mongoose.Schema({
-  questionText: String,
-  metaData: {
-    difficultyLevel: String,
-    topic: String,
-    questionType: String,
-    bookTitle: String,
-    authors: String,
-    resources: {
-      imagePath: String,
-      tableDataPath: String
-    }
+// Define the schema for the metadata of each question
+const metaDataSchema = new mongoose.Schema({
+  difficultyLevel: String,
+  topic: String,
+  questionType: String,
+  bookTitle: String,
+  authors: String,
+  chapterPageNumber: String,
+  resources: {
+    imagePath: String,
+    tableDataPath: String
   }
 });
 
+// Define the schema for each question
+const questionSchema = new mongoose.Schema({
+  questionText: String,
+  metaData: metaDataSchema
+});
+
+// Define the schema for each chapter
 const chapterSchema = new mongoose.Schema({
   chapterName: String,
-  chapterPageNumbers: [String],
   questions: [questionSchema]
 });
 
+// Define the schema for each subject
 const subjectSchema = new mongoose.Schema({
   subjectName: String,
   chapters: [chapterSchema]
 });
 
+// Define the main class schema that includes all subjects
 const classSchema = new mongoose.Schema({
   className: String,
   subjects: [subjectSchema]
 });
 
+// Create the model based on the class schema
 const ClassModel = mongoose.model('ClassModel', classSchema);
 
 module.exports = ClassModel;
