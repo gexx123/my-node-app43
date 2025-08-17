83afc72b96910c2fcb439e32e9c743c54d1f6c51
gexx123 <133238256+gexx123@users.noreply.github.com>
2025-08-17 13:57:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,25 +6,20 @@ const QuestionRoutes = require('./routes/RouteQuestion');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Prefer env var; fallback uses question_bank DB as required
-const MONGO_URI = process.env.MONGODB_URI
-  || 'mongodb+srv://tunwalhimanshu:kCyfmscb2spY14yG@paperbot.6vhle9d.mongodb.net/schoolData?retryWrites=true&w=majority&appName=paperbot';
+// MongoDB connection string
+const MONGO_URI = 'mongodb+srv://tunwalhimanshu:kCyfmscb2spY14yG@paperbot.6vhle9d.mongodb.net/schoolData?retryWrites=true&w=majority&appName=paperbot';
 
-mongoose.connect(MONGO_URI, {
-  // mongoose v6+ ignores useNewUrlParser/useUnifiedTopology but safe
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
 
 app.use(cors());
-app.use(express.json({ limit: '2mb' }));
+app.use(express.json());
 
-// Mount flat questions routes
 app.use('/api', QuestionRoutes);
 
-app.get('/', (_req, res) => {
-  res.send('Question Bank API is running');
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
